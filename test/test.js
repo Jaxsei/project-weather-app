ef66cfb4cc09af1a6ecc68e4f7ad6c9fa296bb51
jaxsei <thakuriamohan18@gmail.com>
2024-08-21 20:54:00 +0530

    Add test files

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// test/test.js
+const assert = require('assert');
+
+describe('Array', function() {
+  it('should start empty', function() {
+    const arr = [];
+    assert.strictEqual(arr.length, 0);
+  });
+});
